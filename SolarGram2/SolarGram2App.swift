e16307fd09ac808115c35faa4a1de645ca5b4acc
rachelqian777 <jiaqiqia@usc.edu>
2023-03-04 13:38:23 -0800

    Initial Commit

---
+++ SolarGram2/SolarGram2App.swift
@@ -0,0 +1,17 @@
+//
+//  SolarGram2App.swift
+//  SolarGram2
+//
+//  Created by Rachel Qian on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SolarGram2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
