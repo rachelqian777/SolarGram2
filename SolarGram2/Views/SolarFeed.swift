c18f3e54e67c49bdc98ddd453266e1d94fe01002
rachelqian777 <jiaqiqia@usc.edu>
2023-03-05 09:39:35 -0800

    Week 1 final

--- SolarGram2/Views/SolarFeed.swift
+++ SolarGram2/Views/SolarFeed.swift
@@ -13,7 +13,7 @@ struct SolarFeed: View {
         ScrollView{
             SinglePost(profile:"Rachel", image: "rachelpicture1", description: "Some traveling memories...", name: "Rachel Qian")
             SinglePost(profile:"Rachel", image: "rachelpicture2", description: "Some more traveling memories...", name: "Rachel Qian")
-            SinglePost(profile:"Jane", image: "picture8", description: "Last year's travel log", name: "Jane M.")
+            SinglePost(profile:"Jane", image: "picture6", description: "Travel painting", name: "Jane M.")
             SinglePost(profile:"Iris", image: "picture7", description: "Iris's painting progress", name: "Iris K.")
             SinglePost(profile:"Rachel", image: "rachelpicture3", description: "Mountains sunset", name: "Rachel Qian")
             SinglePost(profile:"Rachel", image: "rachelpicture4", description: "More  mountains", name: "Rachel Qian")
